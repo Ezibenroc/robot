27800d9ab2313f5c0050bd875bad3d477e8f5010
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-10-16 15:46:44 +0200

    First test. Comment given factory functions.

--- factory.erl
+++ factory.erl
@@ -1,18 +1,23 @@
 -module(factory).
--export([spawnFactory/3,spawnLoop/3,test/2,killFactory/0]).
+-export([spawnFactory/3,spawnLoop/3,test/2,killFactory/0,spawnRobots/4]).
 
+% Spawn a factory registered under the name 'factory', which will spawn robots
+% executing the function Module:Name with parameters [DefaultParameters|X] where
+% X is the name of the robot.
 spawnFactory(Module, Name, DefaultParameters) ->
 	case whereis(factory) of
 		undefined -> register(factory,spawn(factory,spawnLoop,[Module,Name,DefaultParameters]));
 		_ -> io:fwrite("<spawnFactory> Killing old factory...~n"), killFactory(), spawnFactory(Module,Name,DefaultParameters)
 	end.
 
+% Loop executed by the factory (see above).
 spawnLoop(Module,Name,DefaultParameters) ->
 	receive {spawn,Amount,Pid} ->
 		Pid ! {spawned, spawnRobots(Amount,Module,Name,DefaultParameters)}
 	end,
 	spawnLoop(Module,Name,DefaultParameters).
 
+% Spawn Amount robots (see above for more details).
 spawnRobots(0,_,_,_) -> [];
 spawnRobots(Amount,Module,Name,DefaultParameters) ->
 	(UniqueName =
@@ -23,12 +28,14 @@ spawnRobots(Amount,Module,Name,DefaultParameters) ->
 	register(UniqueName, spawn(Module,Name,lists:concat([DefaultParameters,[UniqueName]]))),
 	[UniqueName | spawnRobots(Amount-1,Module,Name,DefaultParameters)].
 
+% Kill the factory.
 killFactory() ->
 	case whereis(factory) of
 		undefined -> error;
 		Pid -> exit(Pid,kill)
 	end.
 
+% WTF does it do ?
 test(Atom,Name) ->
 	io:fwrite("<~s> ~s~n",[Name,Atom]),
 	receive impossiblemessage717 -> haha after 10000 -> test(Atom,Name) end.
