de12aab777e0fcf044080bd3c85685f07c92fd17
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-10-25 19:26:11 +0200

    Add comments.

--- myArbiter.erl
+++ myArbiter.erl
@@ -3,8 +3,13 @@
 -export([handleAction/4,start/0]).
 -include_lib("eunit/include/eunit.hrl").
 
+% HandleAction function for the arbiter.
+% Handled actions:
+%   [move,[X1,Y1],[X2,Y2]]
+%   [handlemove,Status,[X1,Y1],[X2,Y2]] (problem of security?)
 handleAction(Pid, Params, State, _) ->
     case Params of
+        % MOVE
         % Delay of 250ms for a move
         [move,[X1,Y1],[X2,Y2]] ->
             Start = myLists:get_(X1,Y1,State),
@@ -17,12 +22,14 @@ handleAction(Pid, Params, State, _) ->
                     -> timer:send_after(250,self(),{arbiterRequest,Pid,action,[handlemove,blocked,[X1,Y1],[X2,Y2]]}), State;
                 true -> timer:send_after(250,self(),{arbiterRequest,Pid,action,[handlemove,ok,[X1,Y1],[X2,Y2]]}), myLists:set_(X2,Y2,"x",myLists:set_(X1,Y1,"x",State))
             end;
+        % HANDLEMOVE
         [handlemove,Action,[X1,Y1],[X2,Y2]] -> 
             case Action of
                 invalid -> Pid ! invalid, State ;
                 blocked -> Pid ! blocked, State ;
                 ok -> Pid ! ok, myLists:set_(X2,Y2,"r",myLists:set_(X1,Y1,"o",State))
             end;
+        % MISC
         _ -> io:fwrite("HandleActions: received unknown Params.\n"), State
     end.
 
