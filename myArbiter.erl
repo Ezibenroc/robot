80f3fb907f73c61169fa6cfbf455ae3d5d9fdf02
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-11-05 12:18:41 +0100

    Implement enter.

--- myArbiter.erl
+++ myArbiter.erl
@@ -2,6 +2,11 @@
 -import(arbiter,[startArbiter/4]).
 -export([handleAction/4,start/0]).
 -include_lib("eunit/include/eunit.hrl").
+%-define(TIME_MOVE, 250).
+%-define(TIME_ENTER, 1000).
+-define(TIME_MOVE, 5).
+-define(TIME_ENTER, 10).
+
 
 % HandleAction function for the arbiter.
 % Handled actions:
@@ -25,17 +30,40 @@ handleAction(Pid, Params, State, _) ->
             if
                 (abs(X1-X2) > 1) or (abs(Y1-Y2) > 1)
                     or (Start =/= "r")
-                    ->  timer:send_after(250,self(),{arbiterRequest,Pid,action,[handlemove,invalid,[X1,Y1],[X2,Y2]]}), State;
+                    ->  timer:send_after(?TIME_MOVE,self(),{arbiterRequest,Pid,action,[handlemove,invalid,[X1,Y1],[X2,Y2]]}), State;
                 End =/= "o" 
-                    -> timer:send_after(250,self(),{arbiterRequest,Pid,action,[handlemove,blocked,[X1,Y1],[X2,Y2]]}), State;
-                true -> timer:send_after(250,self(),{arbiterRequest,Pid,action,[handlemove,ok,[X1,Y1],[X2,Y2]]}), {Entry,Exit,myLists:set_(X2,Y2,"x",myLists:set_(X1,Y1,"x",Map))}
+                    -> timer:send_after(?TIME_MOVE,self(),{arbiterRequest,Pid,action,[handlemove,blocked,[X1,Y1],[X2,Y2]]}), State;
+                true -> timer:send_after(?TIME_MOVE,self(),{arbiterRequest,Pid,action,[handlemove,ok,[X1,Y1],[X2,Y2]]}), {Entry,Exit,myLists:set_(X2,Y2,"x",myLists:set_(X1,Y1,"x",Map))}
             end;
         % HANDLEMOVE
-        [handlemove,Action,[X1,Y1],[X2,Y2]] -> 
+        [handlemove,Action,[X1,Y1],[X2,Y2]] ->
             case Action of
                 invalid -> Pid ! invalid, State ;
                 blocked -> Pid ! blocked, State ;
                 ok -> Pid ! ok, {Entry,Exit,myLists:set_(X2,Y2,"r",myLists:set_(X1,Y1,"o",Map))}
+            end;
+        % ENTER
+        [enter,EntryPoint,RobotName] ->
+        {Xentry,Yentry,EntryState} = try lists:nth(EntryPoint,Entry) of
+                {X,Y} -> {X,Y,myLists:get_(X,Y,Map)};
+                _ -> error
+            catch
+                error:function_clause -> {invalid,invalid,invalid};
+                X -> X
+            end,
+        if
+            (EntryState =:= "invalid")
+                ->  timer:send_after(?TIME_ENTER,self(),{arbiterRequest,Pid,action,[handleenter,invalid,[Xentry,Yentry],RobotName]}), State;
+            (EntryState =/= "o")
+                -> timer:send_after(?TIME_ENTER,self(),{arbiterRequest,Pid,action,[handleenter,blocked,[Xentry,Yentry],RobotName]}), State;
+            true -> timer:send_after(?TIME_ENTER,self(),{arbiterRequest,Pid,action,[handleenter,ok,[Xentry,Yentry],RobotName]}), {Entry,Exit,myLists:set_(Xentry,Yentry,"x",Map)}
+        end;
+        % HANDLEENTER
+        [handleenter,Action,[Xentry,Yentry],_] ->
+            case Action of
+                invalid -> Pid ! invalid, State ;
+                blocked -> Pid ! blocked, State ;
+                ok -> Pid ! ok, {Entry,Exit,myLists:set_(Xentry,Yentry,"r",Map)}
             end;
         % MISC
         _ -> io:fwrite("HandleActions: received unknown Params.\n"), State
