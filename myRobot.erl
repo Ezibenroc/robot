09fd14b2920ab839201c9b0d988c90c7b2591aeb
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-11-09 19:14:12 +0100

    Add state in robots.

--- myRobot.erl
+++ myRobot.erl
@@ -1,6 +1,6 @@
 -module(myRobot).
 -import(robotUtils,[multiSend/2]).
--export([mainRobot/6]).
+-export([mainRobot/7]).
 -include_lib("eunit/include/eunit.hrl").
 
 -define(TIME_REC, 50).
@@ -11,16 +11,16 @@
 % When one robot receive {terminate_request}, he handle all its last messages, then send an ack and quit.
 
 % Function called to start a robot.
-mainRobot([X,Y],EntryPoints,ExitPoints,Termination,TerminationRequester,ID) ->
+mainRobot(State,[X,Y],EntryPoints,ExitPoints,Termination,TerminationRequester,ID) ->
     receive
-        {PID,terminate_request} -> mainRobot([X,Y],EntryPoints,ExitPoints,true,[PID|TerminationRequester],ID)
+        {PID,terminate_request} -> mainRobot(State,[X,Y],EntryPoints,ExitPoints,true,[PID|TerminationRequester],ID)
     after ?TIME_REC ->
         if
             Termination -> robotUtils:multiSend(TerminationRequester,{self(),ackTerminate}), terminated;
-            true -> noMessage([X,Y],EntryPoints,ExitPoints,Termination,TerminationRequester,ID)
+            true -> noMessage(State,[X,Y],EntryPoints,ExitPoints,Termination,TerminationRequester,ID)
         end
     end.
 
 % Function called when there is no message to read, and process do not need to terminate.
-noMessage([X,Y],EntryPoints,ExitPoints,Termination,TerminationRequester,ID) ->
-    mainRobot([X,Y],EntryPoints,ExitPoints,Termination,TerminationRequester,ID).
+noMessage(State,[X,Y],EntryPoints,ExitPoints,Termination,TerminationRequester,ID) ->
+    mainRobot(State,[X,Y],EntryPoints,ExitPoints,Termination,TerminationRequester,ID).
