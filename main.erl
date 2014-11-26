e785b73078272579b6176f56349c7c6cceeb8a22
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-11-26 19:10:55 +0100

    Correct new map.

--- main.erl
+++ main.erl
@@ -3,6 +3,6 @@
 
 % Function to start everything
 start() ->
-    myArbiter:start(myLists:getState3()),
+    myArbiter:start(myLists:getState4()),
     factory:spawnFactory(myRobot, mainRobot, [init,{-1,-1},[]]),
     factory ! {spawn,10,self()}.
