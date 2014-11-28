97c8fbe60fc10ae1a14d6ae9e780a5f3968dc7e4
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-11-28 23:34:59 +0100

    Add parameter History in robots.

--- myRobot.erl
+++ myRobot.erl
@@ -1,6 +1,6 @@
 -module(myRobot).
 -import(robotUtils,[multiSend/2]).
--export([noMessage/4,mainRobot/4,exploreCell/2,explore/1]).
+-export([mainRobot/5,exploreCell/2,explore/1]).
 -include_lib("eunit/include/eunit.hrl").
 
 -define(TIME_REC, 50).
@@ -12,96 +12,96 @@
 % When one robot receive {terminate_request}, he handle all its last messages, then send an ack and quit.
 
 % Function called to start a robot.
-mainRobot(State,{X,Y},TerminationRequester,ID) ->
+mainRobot(State,History,{X,Y},TerminationRequester,ID) ->
 %    ?debugVal(State),
     receive
-        {PID,terminate_request} -> mainRobot(terminate,{X,Y},[PID|TerminationRequester],ID);
-        {entries,ListEntries} -> handleEntries(State,{X,Y},TerminationRequester,ID,ListEntries);
-        ok -> handleOk(State,{X,Y},TerminationRequester,ID);
-        invalid -> handleInvalid(State,{X,Y},TerminationRequester,ID);
-        blocked -> handleBlocked(State,{X,Y},TerminationRequester,ID);
-        {Content,Message} -> handleInfo(State,{X,Y},TerminationRequester,ID,Content,Message);
+        {PID,terminate_request} -> mainRobot(terminate,History,{X,Y},[PID|TerminationRequester],ID);
+        {entries,ListEntries} -> handleEntries(State,History,{X,Y},TerminationRequester,ID,ListEntries);
+        ok -> handleOk(State,History,{X,Y},TerminationRequester,ID);
+        invalid -> handleInvalid(State,History,{X,Y},TerminationRequester,ID);
+        blocked -> handleBlocked(State,History,{X,Y},TerminationRequester,ID);
+        {Content,Message} -> handleInfo(State,History,{X,Y},TerminationRequester,ID,Content,Message);
         X -> io:fwrite("Robot ~w received unknown message: ~w\n",[ID,X])
     after ?TIME_REC ->
         case State of
             terminate -> robotUtils:multiSend(TerminationRequester,{self(),ackTerminate}), terminated;
-            _ -> noMessage(State,{X,Y},TerminationRequester,ID)
+            _ -> noMessage(State,History,{X,Y},TerminationRequester,ID)
         end
     end.
 
 % Function called when there is no message to read, and process do not need to terminate.
-noMessage(State,{X,Y},TerminationRequester,ID) ->
+noMessage(State,History,{X,Y},TerminationRequester,ID) ->
     case State of
         init -> arbiter ! {arbiterRequest,self(),info,[entry]}, % ask the coordinates of the entry
-            mainRobot(State,{X,Y},TerminationRequester,ID) ;
-        normal -> exploreAndMove(State,{X,Y},TerminationRequester,ID);
-        _ -> mainRobot(State,{X,Y},TerminationRequester,ID)
+            mainRobot(State,History,{X,Y},TerminationRequester,ID) ;
+        normal -> exploreAndMove(State,History,{X,Y},TerminationRequester,ID);
+        _ -> mainRobot(State,History,{X,Y},TerminationRequester,ID)
     end.
 
 % Function called for the reception of entries
-handleEntries(State,{X,Y},TerminationRequester,ID,ListEntries) ->
+handleEntries(State,History,{X,Y},TerminationRequester,ID,ListEntries) ->
     case State of
         init -> random:seed(now()), 
             Entry=trunc(random:uniform()*length(ListEntries))+1,
             arbiter ! {arbiterRequest,self(),action,[enter,Entry,ID]}, % WTF is this bug ?
-            mainRobot({arbiterRequest,enter,Entry,ListEntries},{X,Y},TerminationRequester,ID) ;
-        _ -> mainRobot(State,{X,Y},TerminationRequester,ID)
+            mainRobot({arbiterRequest,enter,Entry,ListEntries},History,{X,Y},TerminationRequester,ID) ;
+        _ -> mainRobot(State,History,{X,Y},TerminationRequester,ID)
     end.
 
-handleOk(State,{X,Y},TerminationRequester,ID) ->
+handleOk(State,History,{X,Y},TerminationRequester,ID) ->
     case State of
         {arbiterRequest,enter,Entry,ListEntries} -> 
             Pos = lists:nth(Entry,ListEntries),
 %            ?debugMsg("ENTER"),
-            mainRobot(normal,Pos,TerminationRequester,ID);
+            mainRobot(normal,History,Pos,TerminationRequester,ID);
         {arbiterRequest,move,Pos} ->
 %            ?debugMsg("MOVE"),
-            mainRobot(normal,Pos,TerminationRequester,ID);
+            mainRobot(normal,History,Pos,TerminationRequester,ID);
         {arbiterRequest,collect,_} ->
 %            ?debugMsg("COLLECT"),
-            mainRobot(normal,{X,Y},TerminationRequester,ID);
-        _ -> mainRobot(State,{X,Y},TerminationRequester,ID)
+            mainRobot(normal,History,{X,Y},TerminationRequester,ID);
+        _ -> mainRobot(State,History,{X,Y},TerminationRequester,ID)
     end.
 
-handleInvalid(State,{X,Y},TerminationRequester,ID) ->
+handleInvalid(State,History,{X,Y},TerminationRequester,ID) ->
     case State of
         {arbiterRequest,enter,_,_} ->
             ?debugMsg("Received invalid in state enter."),
-            mainRobot(init,{X,Y},TerminationRequester,ID);
+            mainRobot(init,History,{X,Y},TerminationRequester,ID);
         {arbiterRequest,move,_} ->
             ?debugMsg("Received invalid in state move."),
-            mainRobot(normal,{X,Y},TerminationRequester,ID);
+            mainRobot(normal,History,{X,Y},TerminationRequester,ID);
         {arbiterRequest,collect,_} ->
             ?debugMsg("Received invalid in state collect."),
-            mainRobot(normal,{X,Y},TerminationRequester,ID);
+            mainRobot(normal,History,{X,Y},TerminationRequester,ID);
         {arbiterRequest,analyze,_} ->
             ?debugMsg("Received invalid in state analyze."),
-            mainRobot(normal,{X,Y},TerminationRequester,ID);
+            mainRobot(normal,History,{X,Y},TerminationRequester,ID);
         _ -> ?debugMsg("Received invalid in unknown state."),
-            mainRobot(State,{X,Y},TerminationRequester,ID)
+            mainRobot(State,History,{X,Y},TerminationRequester,ID)
     end.
 
-handleBlocked(State,{X,Y},TerminationRequester,ID) ->
+handleBlocked(State,History,{X,Y},TerminationRequester,ID) ->
     case State of
         {arbiterRequest,enter,Entry,_} -> 
             arbiter ! {arbiterRequest,self(),action,[enter,Entry,ID]},
-            mainRobot(State,{X,Y},TerminationRequester,ID);
+            mainRobot(State,History,{X,Y},TerminationRequester,ID);
         {arbiterRequest,move,_} ->
-            mainRobot(normal,{X,Y},TerminationRequester,ID);
-        _ -> mainRobot(State,{X,Y},TerminationRequester,ID)
+            mainRobot(normal,History,{X,Y},TerminationRequester,ID);
+        _ -> mainRobot(State,History,{X,Y},TerminationRequester,ID)
     end.
 
-handleInfo(State,{X,Y},TerminationRequester,ID,Content,_) ->
+handleInfo(State,History,{X,Y},TerminationRequester,ID,Content,_) ->
     case State of
         {arbiterRequest,analyze,Pos} ->
                 case Content of
                     gold -> arbiter ! {arbiterRequest,self(),action,[collect,{X,Y},Pos,?STUDENT]},
-                        mainRobot({arbiterRequest,collect,Pos},{X,Y},TerminationRequester,ID);
+                        mainRobot({arbiterRequest,collect,Pos},History,{X,Y},TerminationRequester,ID);
                     empty -> arbiter ! {arbiterRequest,self(),action,[move,{X,Y},Pos]},
-                        mainRobot({arbiterRequest,move,Pos},{X,Y},TerminationRequester,ID);
-                    _ -> mainRobot(normal,{X,Y},TerminationRequester,ID)
+                        mainRobot({arbiterRequest,move,Pos},History,{X,Y},TerminationRequester,ID);
+                    _ -> mainRobot(normal,History,{X,Y},TerminationRequester,ID)
                 end;
-        _ -> mainRobot(State,{X,Y},TerminationRequester,ID)
+        _ -> mainRobot(State,History,{X,Y},TerminationRequester,ID)
     end.
 
 % Explore the cell NewPos, assuming that the robot is located in Pos.
@@ -147,13 +147,13 @@ explore({X,Y}) ->
 
 % Explore all the neighbours of the cell {X,Y}.
 % Move randomly toward one empty cell.
-exploreAndMove(State,{X,Y},TerminationRequester,ID) ->
+exploreAndMove(State,History,{X,Y},TerminationRequester,ID) ->
     ListPos = explore({X,Y}),
     case ListPos of
-        [] -> mainRobot(State,{X,Y},TerminationRequester,ID);
+        [] -> mainRobot(State,History,{X,Y},TerminationRequester,ID);
         _ -> 
             Next=trunc(random:uniform()*length(ListPos))+1,
             NextPos = lists:nth(Next,ListPos),
             arbiter ! {arbiterRequest,self(),action,[move,{X,Y},NextPos]},
-            mainRobot({arbiterRequest,move,NextPos},{X,Y},TerminationRequester,ID)
+            mainRobot({arbiterRequest,move,NextPos},History,{X,Y},TerminationRequester,ID)
     end.
